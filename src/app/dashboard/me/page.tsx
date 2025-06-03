7b3e4e0e95296a5d82b10fce5e6e753c2ab65bb6
felipe mascena <felipe.mascena.dev@gmail.com>
2025-06-03 00:53:49 -0300

    feat: adicionando Toasters e finalizando a page Me

--- src/app/dashboard/me/page.tsx
+++ src/app/dashboard/me/page.tsx
@@ -1,6 +1,7 @@
-import { auth } from '@/lib/auth';
-import { redirect } from 'next/navigation';
-import { UrlPreview } from './_components/url';
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { UrlPreview } from "./_components/url";
+import { CardProfile } from "./_components/card-profile";
 
 export default async function Me() {
   const session = await auth();
@@ -20,9 +21,15 @@ export default async function Me() {
 
   return (
     <main className="w-full h-full flex gap-4 flex-col items-center p-4">
-      <section className="w-full flex lg:flex-row flex-col lg:items-center mx-auto bg-zinc-900 rounded-md p-4 gap-2">
+      <section
+        className="w-full flex lg:flex-row flex-col lg:items-center mx-auto bg-zinc-900 rounded-md p-4 gap-2"
+      >
         <UrlPreview username={userData.username} />
       </section>
-    </main>
+
+
+      <CardProfile user={userData} />
+
+    </main >
   );
 }
